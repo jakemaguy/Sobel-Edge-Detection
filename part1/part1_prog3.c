fd728c8edf67cc9e29ceee3e03274e7c856014df
jakemaguy <36657294+jakemaguy@users.noreply.github.com>
2018-04-10 11:55:37 -0400

    Update part1_prog3.c

--- part1/part1_prog3.c
+++ part1/part1_prog3.c
@@ -1,3 +1,5 @@
+// part1_prog3.c - Part 1 Sobel Filter
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,6 +11,7 @@ int main()
 	// argument supplies string to where the file is located.
     readPGM("../images/mri.pgm");
 	
+	// MIN/MAX variable Definitons
 	float max_horiz_sobel = (float)input_image.matrix[1][1];
 	float min_horiz_sobel = max_horiz_sobel;
 	float max_vert_sobel = max_horiz_sobel;
@@ -16,12 +19,16 @@ int main()
 	float max_mag = min_vert_sobel;
 	float min_mag = max_mag;
 	
+	// --------------------- PART 1 - Sobel Filter ------------------------------------
+	
 	// Determines MAX/MIN Values sobel filter, for normalization
 	int i, j; // loop variables
 	unsigned int height = input_image.height;
 	unsigned int width = input_image.width;
 	unsigned char buffer[9];
 
+	// Loops through entire image and determines min/max values 
+	// so the normalization function can convert values (0-255) unsigned char
 	for (i = 1; i < height; i++)
 	{
 		for (j = 1; j < width; j++)
@@ -42,6 +49,8 @@ int main()
 			double dx = ((buffer[0] * -1) + (buffer[3] * -2) + (buffer[6] * -1) + (buffer[5] * 2) + buffer[2] +  buffer[8]);
 			double dy = ((buffer[0] * -1) + (buffer[1] * -2) + (buffer[2] * -1) + buffer[6] + (buffer[7] * 2) + buffer[8]);
 			double magnitude = sqrt((dx * dx) + (dy * dy));
+			
+			// Determines Actual Min/Max values for normalization
 			if (dx < min_horiz_sobel) min_horiz_sobel = dx;
 			if (dx > max_horiz_sobel) max_horiz_sobel = dx;
 			if (dy < min_vert_sobel) min_vert_sobel = dy;
@@ -50,8 +59,13 @@ int main()
 			if (magnitude > max_mag) max_mag = magnitude;
 		}
 	}
-
+	
+	// Edit this variable to change threshold on binary image
 	int threshold = 100;
+	
+	// Loops through entire image and calculates the vertical/horizontal
+	// Sobel filter approximations, then calculates the combined
+	// vertical and horizontal components to create sobel edge
 	for (i = 1; i < height+1; i++)
 	{
 		for (j = 1; j < width+1; j++)
@@ -70,13 +84,18 @@ int main()
 			double dx = ((buffer[0] * -1) + (buffer[3] * -2) + (buffer[6] * -1) + (buffer[5] * 2) + buffer[2] + buffer[8]);
 			double dy = ((buffer[0] * -1) + (buffer[1] * -2) + (buffer[2] * -1) + buffer[6] + (buffer[7] * 2) + buffer[8]);
 			double magnitude = sqrt((dx * dx) + (dy * dy));
-			input_image.horiz_sobel[i-1][j-1] = (unsigned char)255 * (buffer[4] - min_horiz_sobel) / (max_horiz_sobel - min_horiz_sobel);
-			input_image.vert_sobel[i - 1][j - 1] = (unsigned char)255 * (buffer[4] - min_vert_sobel) / (max_vert_sobel - min_vert_sobel);
+			unsigned char horiz = (unsigned char)dx;
+			unsigned char vert = (unsigned char)dy;
+			input_image.horiz_sobel[i-1][j-1] = (unsigned char)255 * (dx - min_horiz_sobel) / (max_horiz_sobel - min_horiz_sobel);
+			input_image.vert_sobel[i - 1][j - 1] = (unsigned char)255 * (dy - min_vert_sobel) / (max_vert_sobel - min_vert_sobel);
+			// Basis for Binary Image
 			if (magnitude >= threshold) input_image.sobel[i][j] = 255;
 			else input_image.sobel[i][j] = 0;
 		}
 	}
 
+	// --------------------- PART 2 - Write Noise-Free Sobel Images ------------------------------------
+	
 	writePGM("../images/horiz_sobel.pgm");
 	printf("Horizontal Sobel Has Been Created\n");
 
@@ -89,6 +108,8 @@ int main()
     return 0;
 }
 
+// READPGM()
+// input arg: const char array that points to name of file to be read
 void readPGM(const char* filename)
 {
 	int ch;
@@ -145,6 +166,8 @@ void readPGM(const char* filename)
 }
 
 
+// WRITEPGM()
+// input arg: const char array that points to name of file to be written
 void writePGM(const char* filename) {
 	char ch;
 	FILE *out_file;
@@ -166,6 +189,8 @@ void writePGM(const char* filename) {
 	{
 		for (j = 0; j < input_image.width; j++)
 		{
+			// Does String Comparisons to deterine which unsigned char array to print from
+			// Definatley could have been done a more elegant/effcient way, sorry 
 			if (strcmp(filename, "../images/vert_sobel.pgm") == 0)
 			{
 				ch = input_image.vert_sobel[i][j];
